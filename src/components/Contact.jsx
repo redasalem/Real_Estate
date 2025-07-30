32a7aaecdd64f4fbb8073a31c2d0ece180cb4b86
Reda Salem <redasalem287@gmail.com>
2025-07-31 01:44:03 +0300

    Finish component conact us

---
+++ src/components/Contact.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { toast } from 'react-toastify';
+
+const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key",`${import.meta.env.VITE_WEB3FORMS_ACCESS_KEY}`);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+        setResult("");
+        toast.success('Form Submitted Successfully');
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message)
+      setResult('');
+    }
+  };
+  return (
+    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='contact'>
+         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4
+         decoration-1 under font-light'>With Us</span></h1>
+        <p className='text-gray-500 max-w-80 text-center mb-8 mx-auto'>Ready to Make a Move ? let's Build Your future Together</p>
+
+        <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
+            <div className='flex flex-wrap'>
+                <div className='w-full md:w-1/2 text-left'>
+                    Your Name
+                <input name='Name' className='w-full border border-gray-300 rounded py-3 px-4 mt-2' type="text" placeholder='Your Name' required />
+                </div>
+                  <div className='w-full md:w-1/2 text-left md:pl-4'>
+                    Your Email
+                <input name='Email' className='w-full border border-gray-300 rounded py-3 px-4 mt-2' type="email" placeholder='Your Email' required />
+                </div>
+            </div>
+            <div className='my-6 text-left'>
+                message
+                <textarea
+                className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none'
+                 name="message" placeholder='message' required></textarea>
+
+            </div>
+            <button 
+            className='bg-blue-600 text-white py-2 px-12 mb-10 rounded cursor-pointer'
+            >{result ? result :'Send Message'}</button>
+
+        </form>
+        </div>
+  )
+}
+
+export default Contact
